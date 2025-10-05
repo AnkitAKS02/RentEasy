da208409f746b03d1f6a1f9d179121a5a64d3a64
mithun kumar <mihtunraj9703@gmail.com>
2025-10-05 23:40:11 +0530

    add database

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from './lib/db.js'
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("hello world!");
+});
 
-app.listen(() => {
+app.listen(PORT,() => {
     connectDB();
     console.log("Server running on PORT",PORT);
 })
